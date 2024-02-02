20971bf765dd3ea7127265edebf429c84f153e3c
Dhruv Khoradiya <dhruv.khoradiya@etatvasoft.com>
2024-02-02 16:53:48 +0530

    basic Changes

--- newWPFCrudApp/obj/Debug/net8.0-windows/newWPFCrudApp.AssemblyInfo.cs
+++ newWPFCrudApp/obj/Debug/net8.0-windows/newWPFCrudApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newWPFCrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38411149de3ddfe898ad912570688be5916b0d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("newWPFCrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newWPFCrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
