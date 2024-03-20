2b2eebf4815b2b5354a24f92c2bef5beba15741b
Dhruv Khoradiya <dhruv.khoradiya@etatvasoft.com>
2024-03-20 12:04:16 +0530

    minor changes

--- newWPFCrudApp/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ newWPFCrudApp/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -123,7 +123,7 @@ namespace newWPFCrudApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/newWPFCrudApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/newWPFCrudApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
